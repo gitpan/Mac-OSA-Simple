f5ed15b76d10592146de36526f786b59d15c845b
Chris Nandor <cnandor@cpan.org>
1999-03-09 22:44:38 +0000

    Import of CNANDOR/Mac-OSA-Simple-0.10 from CPAN.
    gitpan-cpan-distribution: Mac-OSA-Simple
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         CNANDOR/Mac-OSA-Simple-0.10.tar.gz
    gitpan-cpan-author:       CNANDOR
    gitpan-cpan-maturity:     released

--- Simple.pm
+++ Simple.pm
@@ -1,7 +1,8 @@
 package Mac::OSA::Simple;
 
 use strict;
-use vars qw($VERSION @ISA @EXPORT);
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS
+    %ScriptComponents);
 use Mac::Components;
 use Mac::OSA;
 use Mac::AppleEvents;
@@ -9,48 +10,115 @@ use Exporter;
 use Carp;
 
 @ISA = qw(Exporter AutoLoader);
-@EXPORT = qw(frontier applescript osa_script);
-$VERSION = sprintf("%d.%02d", q$Revision: 0.02 $ =~ /(\d+)\.(\d+)/);
+@EXPORT = qw(compiled frontier applescript osa_script
+    compile_applescript compile_frontier compile_osa_script
+    %ScriptComponents);
+@EXPORT_OK = @Mac::OSA::EXPORT;
+%EXPORT_TAGS = (all => [@EXPORT, @EXPORT_OK]);
+$VERSION = '0.10';
 
-my(%Comp);
+tie %ScriptComponents, 'Mac::OSA::Simple::Components';
 
-sub frontier    { _doscript($_[0], 'LAND') }
-sub applescript { _doscript($_[0], 'ascr') }
-sub osa_script  { _doscript(@_[0, 1])      }
+sub frontier            { _doscript($_[0], 'LAND') }
+sub applescript         { _doscript($_[0], 'ascr') }
+sub osa_script          { _doscript(@_[0, 1])      }
+
+sub compile_frontier    { _compile_script($_[0], 'LAND') }
+sub compile_applescript { _compile_script($_[0], 'ascr') }
+sub compile_osa_script  { _compile_script(@_[0, 1])      }
+
+sub execute {
+    my($self, $value, $return) = ($_[0], '', '');
+
+    $value = OSAExecute($self->{COMP}, $self->{ID}, 0, 0);
+    if ($value) {
+        $return = OSADisplay($self->{COMP}, $value, 'TEXT', 0)
+            or croak $^E;
+        OSADispose($self->{COMP}, $value);
+    }
+    ($self->{RETURN}) = ($return ? (AEPrint($return) =~ /^Ò(.*)Ó$/) : 1);
+    $self->{RETURN};
+}
+
+sub dispose {
+    my $self = shift;
+    if ($self->{ID} && $self->{COMP}) {
+        OSADispose($self->{COMP}, $self->{ID});
+        delete $self->{ID};
+    }
+    if ($self->{SCRIPT}) {
+        AEDisposeDesc($self->{SCRIPT});
+        delete $self->{SCRIPT};
+    }
+    1;
+}
+
+sub compiled {
+    my($self, $script, $data) = @_;
+    $script = OSAStore(@$self{qw(COMP ID)}, 'scpt', 0) or croak $^E;
+    $data = $script->data->get;
+    AEDisposeDesc $script;
+    $data;
+}
+
+sub _compile_script {
+    my($text, $c, $comp, $script, $self) = @_;
+    $self = bless {COMP => $ScriptComponents{$c},
+        TEXT => $text, TYPE => $c}, __PACKAGE__;
+    $self->_compile;
+}
 
 sub _doscript {
-	my($script, $value, $return, $text, $comp);
-
-	if (!defined($Comp{$_[1]})) {	# get component instance
-		$Comp{$_[1]} =
-			OpenDefaultComponent(kOSAComponentType(), $_[1]) or croak $^E;
-	}
-	$comp = $Comp{$_[1]};
-
-	$script = _compile($_[0], $comp);
-	$value = OSAExecute($comp, $script, 0, 0);
-	if ($value) {
-		$return = OSADisplay($comp, $value, 'TEXT', 0)	or croak $^E;
-		OSADispose($comp, $value);
-	}
-	OSADispose($comp, $script);
-
-	return $return ? (AEPrint($return) =~ /^Ò(.*)Ó$/) : 1;
+    my($text, $c, $self, $return) = @_;
+    $self = _compile_script($text, $c);
+    $return = $self->execute;
+    $self->dispose;
+    $return;
 }
 
 sub _compile {
-	my($script, $id);
-	$script = AECreateDesc('TEXT', $_[0])	or croak $^E;
-	$id = OSACompile($_[1], $script, 0)		or croak $^E;
-	AEDisposeDesc($script);
-	return $id;
+    my $self = shift;
+    my($text, $comp, $script, $id) = @_;
+    $self->{SCRIPT} = AECreateDesc('TEXT', $self->{TEXT}) or croak $^E;
+    $self->{ID} = OSACompile($self->{COMP}, $self->{SCRIPT}, 0)
+        or croak $^E;
+    $self;
+}
+
+sub DESTROY {
+    my $self = shift;
+    if (exists($self->{ID}) || exists($self->{SCRIPT})) {
+        $self->dispose;
+    }
 }
 
 END {
-	foreach my $comp (keys %Comp) {
-		CloseComponent($Comp{$comp});
-	}
+    foreach my $comp (keys %ScriptComponents) {
+        CloseComponent($ScriptComponents{$comp});
+    }
 }
+
+package Mac::OSA::Simple::Components;
+
+BEGIN {
+    use Carp;
+    use Tie::Hash ();
+    use Mac::Components;
+    use Mac::OSA;
+    use vars qw(@ISA);
+    @ISA = qw(Tie::StdHash);
+}
+
+sub FETCH {
+    my($self, $comp) = @_;
+    if (!$self->{$comp}) {
+        $self->{$comp} = 
+            OpenDefaultComponent(kOSAComponentType(), $comp)
+            or croak $^E;
+    }
+    $self->{$comp};
+}
+
 
 1;
 __END__
@@ -61,35 +129,62 @@ Mac::OSA::Simple - Simple access to Mac::OSA
 
 =head1 SYNOPSIS
 
-	#!perl -wl
-	use Mac::OSA::Simple;
-	osa_script(<<'EOS', 'LAND');
-	  dialog.getInt ("Duration?",@examples.duration);
-	  dialog.getInt ("Amplitude?",@examples.amplitude);
-	  dialog.getInt ("Frequency?",@examples.frequency);
-	  speaker.sound (examples.duration, examples.amplitude, examples.frequency)
-	EOS
+    #!perl -wl
+    use Mac::OSA::Simple;
+    osa_script(<<'EOS', 'LAND');
+      dialog.getInt ("Duration?",@examples.duration);
+      dialog.getInt ("Amplitude?",@examples.amplitude);
+      dialog.getInt ("Frequency?",@examples.frequency);
+      speaker.sound (examples.duration, examples.amplitude,
+          examples.frequency)
+    EOS
 
-	print frontier('clock.now()');
+    print frontier('clock.now()');
 
-	applescript('beep 3');
+    applescript('beep 3');
 
 =head1 DESCRIPTION
 
-Allows simple access to Mac::OSA.  Just pass the script to the function for C<frontier()> or
-C<applescript()>.  Or, pass the script and the four-character component ID to C<osa_script()>.
-Functions return a value if there is one, or 1 if successful and there is no value.
+Allows simple access to Mac::OSA.  Just pass the script to the function
+for C<frontier()> or C<applescript()>.  Or, pass the script and the
+four-character component ID to C<osa_script()>.   Functions return a
+value if there is one, or 1 if successful and there is no value.
 
-Hm.  Should C<frontier()> and/or C<osa_script($script, 'LAND')> launch Frontier if it is not
-running?
+[Hm.  Should C<frontier()> and/or C<osa_script($script, 'LAND')> launch
+Frontier if it is not running?]
 
-=head1 EXPORT
+Also note that you can get the raw data of a compiled script with:
 
-Exports functions C<frontier()>, C<applescript()>, C<osa_script()>.
+    $script = compile_applescript($script_text);
+    $raw_data = $script->compiled;
+
+(There are also C<compile_frontier> and C<compile_osa_script>).
+
+C<$script> there is an object with parameters like C<SCRIPT> (the
+AEDesc containing the compiled script), C<COMP> (the scripting component),
+C<ID> (the script ID), and C<TEXT> (the text of the script).
+
+You can manually dispose of the OSA script and the AEDesc with
+C<$script-E<gt>dispose>, or let it get disposed of during object destruction.
+
+You can access scripting components via the tied hash C<%ScriptComponents>
+which is automatically exported.  Components are only opened if they have not
+been already, and are closed when the program exits.
+
+
+=head1 TODO
+
+Add OSALoad stuff so compiled scripts from resource forks etc.
+can files can be used.
+
 
 =head1 HISTORY
 
 =over 4
+
+=item v0.10, Tuesday, March 9, 1999
+
+Added lots of stuff to get compiled script data.
 
 =item v0.02, May 19, 1998
 
@@ -102,7 +197,7 @@ Here goes ...
 Chris Nandor F<E<lt>pudge@pobox.comE<gt>>
 http://pudge.net/
 
-Copyright (c) 1998 Chris Nandor.  All rights reserved.  This program is free 
+Copyright (c) 1999 Chris Nandor.  All rights reserved.  This program is free 
 software; you can redistribute it and/or modify it under the same terms as 
 Perl itself.  Please see the Perl Artistic License.
 
@@ -112,6 +207,6 @@ Mac::OSA, Mac::AppleEvents, Mac::AppleEvents::Simple, macperlcat.
 
 =head1 VERSION
 
-Version 0.02 (27 May 1998)
+Version 0.10 (Tuesday, March 9, 1999)
 
 =cut
